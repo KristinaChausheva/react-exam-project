7513a4900f8232eb5d3ea971c725dede0a5506e3
Kristina Chausheva <52411816+KristinaChausheva@users.noreply.github.com>
2021-04-03 21:06:12 +0300

    adding nav ul

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 
 
 import Logo from '../Logo/Logo'
+import Nav from '../Header/Nav/Nav'
 
 function Header (props){
     return (
     <header>
         <div>MENU</div>
         <Logo />
+        <Nav />
         <nav> ... </nav>
     </header>
     )
---
+++ src/components/Header/Nav/Nav.js
@@ -0,0 +1,10 @@
+const nav = (props) => (
+    <ul>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Blog</li>
+        <li>Contact</li>
+    </ul>
+)
+
+export default nav
